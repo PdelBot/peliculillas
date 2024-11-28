9819133b469d679ac526b9d2934e1d14fbf8a2d1
Pedro <sanchez.deped24@triana.salesianos.edu>
2024-11-28 12:07:56 +0100

    feat: Rating de Series

--- PeliculiYas/src/app/components/serie-details/serie-details.component.ts
+++ PeliculiYas/src/app/components/serie-details/serie-details.component.ts
@@ -1,68 +1,108 @@
-import { Component, OnInit } from '@angular/core';
-import { DetailsService } from '../../services/details.service';
-import { Season, SerieDetaisResponse } from '../../models/series-details.interface';
-import { SeasonDetailsResponse } from '../../models/season-details.interface';
-import { ActivatedRoute } from '@angular/router';
-
-@Component({
-  selector: 'app-serie-details',
-  templateUrl: './serie-details.component.html',
-  styleUrl: './serie-details.component.css'
-})
-export class SerieDetailsComponent implements OnInit {
-
-  seriesDetails: SerieDetaisResponse | undefined;
-  seasonsId: Season[] = [];
-  seasons: SeasonDetailsResponse[] = [];
-  selectedSeason: SeasonDetailsResponse | undefined;
-  rating: number = 0;
+  import { Component, OnInit } from '@angular/core';
+  import { DetailsService } from '../../services/details.service';
+  import { Season, SerieDetaisResponse } from '../../models/series-details.interface';
+  import { SeasonDetailsResponse } from '../../models/season-details.interface';
+  import { ActivatedRoute } from '@angular/router';
+  import { RatingService } from '../../services/rating.service';
 
 
+  @Component({
+    selector: 'app-serie-details',
+    templateUrl: './serie-details.component.html',
+    styleUrl: './serie-details.component.css'
+    
+  })
+  export class SerieDetailsComponent implements OnInit {
 
-  constructor(private detailsService: DetailsService, private route: ActivatedRoute) { }
-
-  ngOnInit(): void {
-    const serieId = this.route.snapshot.paramMap.get('id');
-    if (serieId) {
-      this.detailsService.getSeriesDetails(+serieId, 'es-ES').subscribe((response) => {
-        this.seriesDetails = response;
-      });
+    seriesDetails: SerieDetaisResponse | undefined;
+    seasonsId: Season[] = [];
+    seasons: SeasonDetailsResponse[] = [];
+    selectedSeason: SeasonDetailsResponse | undefined;
+    rating: number = 0;
+    userRatingS: number = 0;
 
 
-      this.detailsService.getSeriesDetails(+serieId, 'es-ES').subscribe(data => {
-        if (data.overview) {
-          this.seriesDetails = data;
-          this.seasonsId = data.seasons;
-          this.loadSeasons(+serieId, this.seasonsId);
-          this.rating = (this.seriesDetails.vote_average || 0) / 2;
-        } else {
-          this.detailsService.getSeriesDetails(+serieId, 'en-US').subscribe(englishData => {
-            this.seriesDetails = englishData;
-            this.seasonsId = englishData.seasons;
+
+    constructor(private detailsService: DetailsService, private route: ActivatedRoute, private ratingService: RatingService
+    ) { }
+
+    ngOnInit(): void {
+      const serieId = this.route.snapshot.paramMap.get('id');
+      if (serieId) {
+        this.detailsService.getSeriesDetails(+serieId, 'es-ES').subscribe((response) => {
+          this.seriesDetails = response;
+        });
+
+
+        this.detailsService.getSeriesDetails(+serieId, 'es-ES').subscribe(data => {
+          if (data.overview) {
+            this.seriesDetails = data;
+            this.seasonsId = data.seasons;
             this.loadSeasons(+serieId, this.seasonsId);
-          });
-        }
+            this.rating = (this.seriesDetails.vote_average || 0) / 2;
+          } else {
+            this.detailsService.getSeriesDetails(+serieId, 'en-US').subscribe(englishData => {
+              this.seriesDetails = englishData;
+              this.seasonsId = englishData.seasons;
+              this.loadSeasons(+serieId, this.seasonsId);
+            });
+          }
+        });
+
+            // Cargar la calificación del usuario desde TMDb
+            this.ratingService.getUserRatingS(+serieId).subscribe({
+              next: (response) => {
+                this.userRatingS = response.rated?.value || 0; // Cargar calificación previa si existe
+              },
+              error: (err) => console.error('Error al obtener la calificación del usuario:', err),
+            });
+
+      }
+
+    }
+
+    loadSeasons(serieId: number, seasons: Season[]): void {
+      seasons.forEach(season => {
+        this.detailsService.getSeasonDetails(serieId, season.season_number, 'es-ES').subscribe(data => {
+          this.seasons.push(data);
+        });
       });
     }
 
-  }
+    selectSeason(event: Event, season: SeasonDetailsResponse): void {
+      event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
+      this.selectedSeason = season;
+    }
 
-  loadSeasons(serieId: number, seasons: Season[]): void {
-    seasons.forEach(season => {
-      this.detailsService.getSeasonDetails(serieId, season.season_number, 'es-ES').subscribe(data => {
-        this.seasons.push(data);
+    getImgUrl(path: string): string {
+      const baseUrl = 'https://image.tmdb.org/t/p/w500';
+      return `${baseUrl}${path}`;
+    }
+
+
+  // Guardar la calificación del usuario
+  saveRatingS(newRating: number): void {
+    if (this.seriesDetails) {
+      this.ratingService.saveRatingS(this.seriesDetails.id, newRating).subscribe({
+        next: () => {
+          this.userRatingS = newRating; // Actualizamos la calificación actual en la vista
+          console.log(`Calificación de ${newRating} guardada exitosamente en TMDb`);
+        },
+        error: (err) => console.error('Error al guardar la calificación en TMDb:', err),
       });
-    });
+    }
   }
 
-  selectSeason(event: Event, season: SeasonDetailsResponse): void {
-    event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
-    this.selectedSeason = season;
+  // Borrar la calificación del usuario
+  deleteRatingS(): void {
+    if (this.seriesDetails) {
+      this.ratingService.deleteRatingS(this.seriesDetails.id).subscribe({
+        next: () => {
+          this.userRatingS = 0; // Actualizamos la calificación actual en la vista
+          console.log('Calificación eliminada exitosamente de TMDb');
+        },
+        error: (err) => console.error('Error al eliminar la calificación en TMDb:', err),
+      });
+    }
   }
-
-  getImgUrl(path: string): string {
-    const baseUrl = 'https://image.tmdb.org/t/p/w500';
-    return `${baseUrl}${path}`;
   }
-
-}
--- PeliculiYas/src/app/services/rating.service.ts
+++ PeliculiYas/src/app/services/rating.service.ts
@@ -25,11 +25,32 @@ export class RatingService {
     return this.http.post(url, { value: rating });
   }
 
+  // Borrar la calificación del usuario en TMDb
   deleteRating(movieId: number): Observable<any> {
     const url = `${this.apiUrl}/movie/${movieId}/rating?api_key=${this.apiKey}&session_id=${this.sessionId}`;
     return this.http.delete(url);
   }
 
+  // Obtener la calificación del usuario para una película específica
+  getUserRatingS(serieId: number): Observable<any> {
+    const url = `${this.apiUrl}/tv/${serieId}/account_states?api_key=${this.apiKey}&session_id=${this.sessionId}`;
+    return this.http.get(url);
+  }
+
+  // Guardar la calificación del usuario en TMDb
+  saveRatingS(serieId: number, rating: number): Observable<any> {
+    const url = `${this.apiUrl}/tv/${serieId}/rating?api_key=${this.apiKey}&session_id=${this.sessionId}`;
+    return this.http.post(url, { value: rating });
+  }
+
+  // Borrar la calificación del usuario en TMDb
+  deleteRatingS(serieId: number): Observable<any> {
+    const url = `${this.apiUrl}/tv/${serieId}/rating?api_key=${this.apiKey}&session_id=${this.sessionId}`;
+    return this.http.delete(url);
+  }
+
+
+  
 
 }
   
